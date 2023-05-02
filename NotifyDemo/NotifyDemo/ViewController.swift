3ca4390c897fd0f6f54e74c0849ed410b079095e
poodlepoodle <chammal97@naver.com>
2023-05-02 17:27:26 +0900

    :see_no_evil: :: modify gitignore

---
+++ NotifyDemo/NotifyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotifyDemo
+//
+//  Created by Eojin Choi on 2023/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
