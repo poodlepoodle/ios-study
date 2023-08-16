ba84d510f3e6c1a86c8ac30cf0ffe2b97fcee6a1
poodlepoodle <chammal97@naver.com>
2023-08-16 15:18:55 +0900

    :sparkles: :: practice capturing swiftui view using UIView

--- playground/SwiftUIPlayground/SwiftUIPlayground/Views/UIViewCaptureView.swift
+++ playground/SwiftUIPlayground/SwiftUIPlayground/Views/UIViewCaptureView.swift
@@ -7,14 +7,54 @@
 
 import SwiftUI
 
-struct UIViewCaptureView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension UIView {
+    func asImage(size: CGSize) -> UIImage {
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        return UIGraphicsImageRenderer(size: size, format: format).image { context in
+            layer.render(in: context.cgContext)
+        }
     }
 }
 
-struct UIViewCaptureView_Previews: PreviewProvider {
-    static var previews: some View {
-        UIViewCaptureView()
+func convertViewToData<V>(view: V, size: CGSize, completion: @escaping (Data?) -> Void) where V: View {
+    guard let rootVC = UIApplication.shared.windows.first?.rootViewController else {
+        completion(nil)
+        return
+    }
+    let imageVC = UIHostingController(rootView: view.edgesIgnoringSafeArea(.all))
+    imageVC.view.frame = CGRect(origin: .zero, size: size)
+    DispatchQueue.main.async {
+        rootVC.view.insertSubview(imageVC.view, at: 0)
+        let uiImage = imageVC.view.asImage(size: size)
+        imageVC.view.removeFromSuperview()
+        completion(uiImage.pngData())
+    }
+}
+
+struct UIViewCaptureView: View {
+    @State var imageData: Data?
+
+    var body: some View {
+        VStack {
+            testView
+                .frame(width: 50, height: 50)
+            if let imageData = imageData, let uiImage = UIImage(data: imageData) {
+                Image(uiImage: uiImage)
+            }
+        }
+        .onAppear {
+            convertViewToData(view: testView, size: .init(width: 300, height: 300)) {
+                imageData = $0
+            }
+        }
+    }
+
+    var testView: some View {
+        ZStack {
+            Color.blue
+            Circle()
+                .fill(Color.red)
+        }
     }
 }
